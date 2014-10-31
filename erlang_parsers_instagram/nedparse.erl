5c671d45f363280303104f00c58134fc84a43b07
nedwed123 <neda.amiri5@yahoo.com>
2014-10-31 22:43:20 +0100

    update nedparse

--- erlang_parsers_instagram/nedparse.erl
+++ erlang_parsers_instagram/nedparse.erl
@@ -9,28 +9,26 @@ req(URL)->
 case ibrowse:send_req(URL,[],get) of
 {ok,_,_,Body} -> decorate(Body)
 end.
-
 decorate(B)-> case jiffy:decode(B) of
  {L} ->   
   case lists:keysearch(<<"data">>, 1, L) of
  {value, M} -> case M of
 {_,U}->listparser(U)
-%case M of
- % {_,[H|_]}-> case H of
-%{[{_,Y},{_,X}]}->[X, Y]
-%end
 
- end
-  end
+ end 
+  end 
   end.
 
 listparser(L1)->listparser(L1,[]).
 listparser([],L2)->L2;
-listparser([H|T],L2)-> case H of 
-{[{_,Y},{_,X}]}-> L2++[{X,Y}|listparser(T,L2)]
+listparser([H|T],L2)-> case H of  
+{[{_,Y},{_,X}]}-> L2++[{my_binary_to_list(X),Y}|listparser(T,L2)]
 end.
 
 
 
 
+my_binary_to_list(<<H,T/binary>>) ->
+    [H|my_binary_to_list(T)];
+my_binary_to_list(<<>>) -> []. 
 
